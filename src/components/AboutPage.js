aca8c1846c688cf9fabfad6dd6ab64005825798e
Marco <>
2021-04-11 13:29:28 +0100

    Updated about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -23,7 +23,6 @@ import marcobot from "@assets/marcobot_512.png";
 import { AboutIcon } from "@components/Misc";
 import "@css/DonatePage.css";
 import "@css/HomePage.css";
-
 function AboutPage() {
   const { setAllowLeftPanel } = useContext(F7PanelContext);
 
@@ -42,10 +41,26 @@ function AboutPage() {
           <Link popupClose>Close</Link>
         </NavRight>
       </Navbar>
-      <Block>Marcobot is small search engine for 'pay once' App Store games, games that have no in-app purchases. More info soon.</Block>
-      <Block>Contact me at <Link href={"mailto:marco@marcobot.com"} external>marco@marcobot.com</Link></Block>
-      <Block style={{fontSize:"12px"}}>Marcobot doesn't collect or store any sort of personal data at all. Not even cookies are used on this site
-          so that means I don't have to include any stupid popups.</Block>
+      <Block>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <img
+            src={marcobot}
+            style={{ width: "128px", height: "128px", marginRight: "1rem", borderRadius: "12px" }}
+            alt=""
+          />
+          Marcobot is small search engine for 'pay once' App Store games, games that have no in-app purchases.
+        </div>
+      </Block>
+      <Block>More info soon.</Block>
+      <Block>
+        <Link href={"mailto:marco@marcobot.com"} external>
+          marco@marcobot.com
+        </Link>
+      </Block>
+      <Block style={{ fontSize: "12px" }}>
+        Marcobot doesn't collect or store any sort of personal data at all. Not even cookies are used on this site so
+        that means I don't have to include any stupid popups.
+      </Block>
     </Page>
   );
 }
